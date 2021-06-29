f81a4257da5bcd34c7ff769339fa696a7ffce559
Daeyeon Kim <daebalprime@gmail.com>
2021-06-29 15:14:17 +0900

    test passed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="hello">
-        <h1></h1>
-        <h2>테스트 실패 연습</h2>
+        <h1>{{ msg }}</h1>
         <p>
             For a guide and recipes on how to configure / customize this project,<br />
             check out the
